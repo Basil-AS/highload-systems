097dd1d723b42f7073a24adcaad7fe0fb80fc5bb
Vasilii <71924962+Basil-AS@users.noreply.github.com>
2025-10-16 15:04:41 +0300

    LR4: add planning notes (variant 8) and update variant script for LR4

--- get_variant.py
+++ get_variant.py
@@ -2,10 +2,10 @@ from hashlib import sha3_256
 
 study_group = "211-331"  # Укажите номер учебной группы
 fullname = "Скрыпник Василий Александрович"  # Указаны ФИО
-# Для ЛР3, как в задании
-suffix = "Высоконагруженные системы. Лабораторная работа 3"
+# Для ЛР4, как в задании
+suffix = "Высоконагруженные системы. Лабораторная работа 4"
 
 string_for_hash = f"{study_group} {fullname} {suffix}"
-var_total = 5
+var_total = 10
 variant = int(sha3_256(string_for_hash.encode('utf-8')).hexdigest(), 16) % var_total + 1
 print(f"Ваш вариант: {variant}")
